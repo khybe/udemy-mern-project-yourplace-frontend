b5bce2249da87fee0b5af6bb01f7cd135c74fb89
Khaibar Saadat <khybersaadat93@gmail.com>
2023-03-01 22:31:09 +0100

    Adding a UsersList Page / Component

---
+++ src/user/components/UserItem.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import "./UserItem.css";
+
+const UserItem = () => {};
+
+export default UserItem;
--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,7 +1,18 @@
 import React from "react";
 
+import UsersList from "../components/UsersList";
+
 const Users = () => {
-  return <h2>Users Works!</h2>;
+  const USERS = [
+    {
+      id: "u1",
+      name: "Max",
+      image:
+        "https://www.shutterstock.com/image-vector/silhouette-people-unknown-male-person-260nw-1372192277.jpg",
+      places: 2,
+    },
+  ];
+  return <UsersList items={USERS} />;
 };
 
 export default Users;
