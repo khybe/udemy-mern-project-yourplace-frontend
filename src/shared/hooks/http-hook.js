20287fdf4e31288952c90912295fa17ca68d7871
Khaibar Saadat <khybersaadat93@gmail.com>
2023-03-15 18:03:41 +0100

    Adding Places POST (requires adjustment in  Backend in users-controllers.js for extracting userId to add to the creator property of a Place)

--- src/shared/hooks/http-hook.js
+++ src/shared/hooks/http-hook.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback, useRef, useEffect } from "react";
+import { useState, useCallback, useRef, useEffect } from "react";
 
 export const useHttpClient = () => {
   const [isLoading, setIsLoading] = useState(false);
--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -39,7 +39,7 @@ const Auth = (props) => {
 
     if (isLoginMode) {
       try {
-        await sendRequest(
+        const responseData = await sendRequest(
           "http://localhost:5000/api/users/login",
           "POST",
           JSON.stringify({
@@ -51,11 +51,11 @@ const Auth = (props) => {
           }
         );
 
-        auth.login();
+        auth.login(responseData.user.id);
       } catch (err) {}
     } else {
       try {
-        await sendRequest(
+        const responseData = await sendRequest(
           "http://localhost:5000/api/users/signup",
           "POST",
           JSON.stringify({
@@ -68,7 +68,7 @@ const Auth = (props) => {
           }
         );
 
-        auth.login();
+        auth.login(responseData.userId);
       } catch (err) {}
     }
   };
