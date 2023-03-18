83fa017f10cc579c78417c3b4968241898f3dd54
Khaibar Saadat <khybersaadat93@gmail.com>
2023-03-18 21:23:01 +0100

    Building an ImageUpload Component

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -5,6 +5,7 @@ import Input from "../../shared/components/FormElements/Input";
 import Button from "../../shared/components/FormElements/Button";
 import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner";
 import ErrorModal from "../../shared/components/UIElements/ErrorModal";
+import ImageUpload from "../../shared/components/FormElements/ImageUpload";
 import {
   VALIDATOR_EMAIL,
   VALIDATOR_MINLENGTH,
@@ -119,6 +120,7 @@ const Auth = (props) => {
               onInput={inputHandler}
             />
           )}
+          {!isLoginMode && <ImageUpload center id="image" />}
           <Input
             id="email"
             element="input"
