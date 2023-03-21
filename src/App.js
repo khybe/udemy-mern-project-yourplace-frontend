78fd07351e9126e0d8d8991fddc3a6c3d8d84314
Khaibar Saadat <khybersaadat93@gmail.com>
2023-03-21 21:45:25 +0100

    Finishing Auto-Login & Auto-Logout

--- src/App.js
+++ src/App.js
@@ -14,8 +14,11 @@ import Auth from "./user/pages/Auth";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 import { AuthContext } from "./shared/context/auth-context";
 
+let logoutTimer;
+
 function App() {
   const [token, setToken] = useState(false);
+  const [tokenExpirationDate, setTokenExpirationDate] = useState();
   const [userId, setUserId] = useState(false);
 
   const login = useCallback((uid, token, expirationDate) => {
@@ -23,6 +26,7 @@ function App() {
     setUserId(uid);
     const tokenExpirationDate =
       expirationDate || new Date(new Date().getTime() + 1000 * 60 * 60);
+    setTokenExpirationDate(tokenExpirationDate);
     localStorage.setItem(
       "userData",
       JSON.stringify({
@@ -33,17 +37,29 @@ function App() {
     );
   }, []);
 
-  const logout = useCallback((uid) => {
+  const logout = useCallback(() => {
     setToken(null);
+    setTokenExpirationDate(null);
     setUserId(null);
     localStorage.removeItem("userData");
   }, []);
 
   useEffect(() => {
+    if (token && tokenExpirationDate) {
+      const remainingTime =
+        tokenExpirationDate.getTime() - new Date().getTime();
+      logoutTimer = setTimeout(logout, remainingTime);
+    } else {
+      clearTimeout(logoutTimer);
+    }
+  }, [token, logout, tokenExpirationDate]);
+
+  useEffect(() => {
     const storedData = JSON.parse(localStorage.getItem("userData"));
     if (
-      (storedData && storedData.token,
-      new Date(storedData.expiration) > new Date())
+      storedData &&
+      storedData.token &&
+      new Date(storedData.expiration) > new Date()
     ) {
       login(
         storedData.userId,
